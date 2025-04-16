a72af81294a69b2dc789c81ffb8e8bcc17841f06
Abdulaleem Mughal <mughalabdulaleem8@gmail.com>
2025-04-15 17:42:51 -0900

    SOme Changes in Login.tsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -5,7 +5,6 @@ import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/contants";
-import Toast from "./Toast";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
